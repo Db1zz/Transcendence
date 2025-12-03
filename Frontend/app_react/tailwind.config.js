26edc09aa6370e889c9234372a968d633eab4939
fraumarzhuk <fraumarzhuk@gmail.com>
2025-12-03 21:33:31 +0100

    login page not branded

---
+++ Frontend/app_react/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-green': '#9E9D89',
+        'brand-beige': '#E4D3CF',
+        'brand-peach': '#E2BCB7',
+        'brand-brick': '#B67162'
+      }
+    },
+  },
+  plugins: [],
+}
