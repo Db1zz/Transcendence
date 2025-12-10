c1a3d1b3836ea554cc3f4b348bec50714d70e103
grysha11 <grzhzahar@gmail.com>
2025-12-10 17:53:43 +0100

    init docker-compose for postgresql, backend, frontend

--- Backend/src/main/resources/db/migration/V1__create_user_auth_credentials.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_auth_credentials (
-    user_id BIGINT PRIMARY KEY,
-    user_password VARCHAR(1024) NOT NULL,
-
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
