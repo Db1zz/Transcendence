f08ea8eaab1be4ac3e6b99941fb446273b174ee3
Georgijs Onischenko <xel27@inbox.lv>
2025-12-12 09:58:22 +0200

    add openapi generator

--- Backend/src/main/java/com/anteiku/backend/BackendApplication.java
+++ Backend/src/main/java/com/anteiku/backend/BackendApplication.java
@@ -3,7 +3,7 @@ package com.anteiku.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"controller", "service", "model", "repository"})
+@SpringBootApplication()
 public class BackendApplication {
     static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
