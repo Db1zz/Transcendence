34d866a4e6ce07fbfbf20a967a77586234e06ed9
Georgijs Onischenko <xel27@inbox.lv>
2026-01-24 01:05:41 +0200

    update init.sql script and add user_session table, that will be used later to manage user sessions more securely

--- Backend/docker-entrypoint-initdb.d/init.sql
+++ Backend/docker-entrypoint-initdb.d/init.sql
@@ -14,11 +14,17 @@ CREATE TABLE IF NOT EXISTS users_credentials (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
--- CREATE TABLE IF NOT EXISTS user_sessions (
---     user_id UUID PRIMARY KEY,
---     id UUID NOT NULL,
---     token VARCHAR(255) NOT NULL,
---     device VARCHAR(255) NOT NULL,
---
---     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
--- )
+CREATE TABLE IF NOT EXISTS user_sessions (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    token BYTEA NOT NULL,
+
+    device_id UUID NOT NULL, -- Suka how to get this one i don't blyat know?>?>?>>?>?>
+
+    created_at TIMESTAMPTZ NOT NULL,
+    last_used_at TIMESTAMPTZ DEFAULT NULL,
+    revoked BOOLEAN NOT NULL DEFAULT FALSE,
+    revoked_at TIMESTAMPTZ DEFAULT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
