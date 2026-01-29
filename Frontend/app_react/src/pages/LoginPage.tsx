f203fc51ae2d6485a18043286d289c187b659c28
fraumarzhuk <fraumarzhuk@gmail.com>
2026-01-29 15:10:34 +0100

    incorrect credentials login check

--- Frontend/app_react/src/pages/LoginPage.tsx
+++ Frontend/app_react/src/pages/LoginPage.tsx
@@ -12,6 +12,7 @@ const LoginPage: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const fromLocation = location.state?.from?.pathname || "/";
   useEffect(() => {
@@ -23,6 +24,7 @@ const LoginPage: React.FC = () => {
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
+    setError("");
 
     const success = await login("credentials", { email, password });
 
@@ -30,6 +32,8 @@ const LoginPage: React.FC = () => {
 
     if (success) {
       navigate("/home");
+    } else {
+      setError("incorrect credentials, try again");
     }
   };
 
@@ -63,7 +67,9 @@ const LoginPage: React.FC = () => {
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-3 bg-brand-green placeholder-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-brand-brick"
+              className={`w-full px-4 py-3 bg-brand-green placeholder-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-brand-brick ${
+                error ? "border-2 border-red-600" : ""
+              }`}
               placeholder="enter your email"
             />
           </div>
@@ -76,7 +82,9 @@ const LoginPage: React.FC = () => {
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-3 bg-brand-green placeholder-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-brand-brick"
+              className={`w-full px-4 py-3 bg-brand-green placeholder-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-brand-brick ${
+                error ? "border-2 border-red-600" : ""
+              }`}
               placeholder="enter your password"
             />
           </div>
@@ -86,6 +94,10 @@ const LoginPage: React.FC = () => {
               forgot password?
             </a>
           </div>
+
+          {error && (
+            <div className="text-red-600 text-xl font-roboto">{error}</div>
+          )}
 
           <div className="flex justify-center">
             <Button
