ceb2ab51dcbe5d01d952c0db9b49401cabea3668
Georgijs Onischenko <xel27@inbox.lv>
2026-02-13 21:01:08 +0100

    add channels

--- Backend/docker-entrypoint-initdb.d/init.sql
+++ Backend/docker-entrypoint-initdb.d/init.sql
@@ -15,24 +15,25 @@ CREATE TABLE IF NOT EXISTS users_credentials (
 );
 
 CREATE TABLE IF NOT EXISTS user_sessions (
-    id UUID PRIMARY KEY,
-    refresh_token BYTEA NOT NULL,
-    refresh_token_expires_at TIMESTAMPTZ NOT NULL,
-    access_token BYTEA NOT NULL,
-    access_token_expires_at TIMESTAMPTZ NOT NULL,
+     id UUID PRIMARY KEY,
+     refresh_token BYTEA NOT NULL,
+     refresh_token_expires_at TIMESTAMPTZ NOT NULL,
+     access_token BYTEA NOT NULL,
+     access_token_expires_at TIMESTAMPTZ NOT NULL,
 
-    user_id UUID NOT NULL,
+     user_id UUID NOT NULL,
 
     -- public_key BYTEA DEFAULT NULL, -- idk, shall i keep it?
 
     -- device_id UUID NOT NULL,
 
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    last_active_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    revoked BOOLEAN NOT NULL DEFAULT FALSE,
-    revoked_at TIMESTAMPTZ DEFAULT NULL,
+     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+     last_active_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+     revoked BOOLEAN NOT NULL DEFAULT FALSE,
+     revoked_at TIMESTAMPTZ DEFAULT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+
 );
 
 CREATE TABLE IF NOT EXISTS organizations (
@@ -45,11 +46,11 @@ CREATE TABLE IF NOT EXISTS organizations (
 );
 
 CREATE TABLE IF NOT EXISTS roles (
-  id UUID PRIMARY KEY,
-  organization_id UUID NOT NULL,
-  name VARCHAR NOT NULL,
+    id UUID PRIMARY KEY,
+    organization_id UUID NOT NULL,
+    name VARCHAR NOT NULL,
 
-  FOREIGN KEY (organization_id) REFERENCES organizations(id) ON DELETE CASCADE
+    FOREIGN KEY (organization_id) REFERENCES organizations(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS members (
@@ -70,4 +71,20 @@ CREATE TABLE IF NOT EXISTS member_roles (
 
     FOREIGN KEY (member_id) REFERENCES members(id) ON DELETE CASCADE,
     FOREIGN KEY (role_id) REFERENCES  roles(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS channel (
+    id UUID PRIMARY KEY,
+    organization_id UUID NOT NULL,
+    type VARCHAR NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS channel_overrides (
+    channel_id UUID NOT NULL,
+    entity_type VARCHAR NOT NULL,
+    entity_id UUID NOT NULL,
+
+    PRIMARY KEY (channel_id, entity_id),
+    FOREIGN KEY (channel_id) REFERENCES channel(id) ON DELETE CASCADE
 );
