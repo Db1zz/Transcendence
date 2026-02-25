66dabe053830e00896ab85f5d7897f7fcce6231e
grysha11 <grzhzahar@gmail.com>
2026-02-25 15:35:08 +0100

    wip tokens handler

---
+++ Frontend/app_react/src/utils/api.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:8080/api",
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        const originalReq = error.config;
+
+        if (error.response?.status === 401 && !originalReq._retry) {
+            originalReq._retry = true;
+
+            try {
+                await axios.post(
+                    "http://localhost:8080/api/auth/refresh",
+                    {},
+                    { withCredentials: true }
+                );
+                return api(originalReq);
+            } catch (refreshError) {
+                console.error("Refresh token expired");
+                localStorage.removeItem("user");
+                window.location.href = "/login";
+                return Promise.reject(refreshError);
+            }
+        }
+        return Promise.reject(error);
+    }
+)
+
+export default api;
